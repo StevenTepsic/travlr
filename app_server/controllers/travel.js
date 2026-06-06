1c2e85b6cd2f0dd5c996097d3805b680d9fc006e
StevenTepsic <steven.tepsic@snhu.edu>
2026-06-05 20:48:23 -0400

    fixed travel controller

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
 /* GET TRAVEL VIEW*/
+const fs = require('fs');
+const path = require('path');
+const trips = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/trips.json'), 'utf8'));
+
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
